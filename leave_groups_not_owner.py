6669a5936df613b0b89e3141a7dff0427f8a8fe9
Tanvir Meah <37561141+tanvirmeah@users.noreply.github.com>
2024-06-24 05:10:44 +0600

    Update leave_groups_not_owner.py

--- leave_groups_not_owner.py
+++ leave_groups_not_owner.py
@@ -1,3 +1,29 @@
+"""
+Telegram all Groups and Channels Remover for Non-Owners
+
+This Python script allows you to leave all Telegram groups and channels where you are not the owner using the Telethon library.
+
+Copyright (c) 2024 Tanvir Meah
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import GetDialogsRequest
